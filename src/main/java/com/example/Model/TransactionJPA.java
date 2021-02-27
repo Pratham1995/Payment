f92cbea6f69ef4b36791a4e1cabdc7317a493986
Prathamesh Berde <43159826+Pratham1995@users.noreply.github.com>
2021-02-27 15:03:21 -0600

    First Commit

--- src/main/java/com/example/Model/TransactionJPA.java
+++ src/main/java/com/example/Model/TransactionJPA.java
@@ -1,5 +1,7 @@
 package com.example.Model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,7 +36,7 @@ public class TransactionJPA {
 	@Column(name="PaymentInfo")
 	private String PaymentInfo;
 	@Column(name="ValueDate")
-	private String ValueDate;
+	private Date ValueDate;
 	@Column(name="TransactionStatus")
 	private String TransactionStatus;
 	
