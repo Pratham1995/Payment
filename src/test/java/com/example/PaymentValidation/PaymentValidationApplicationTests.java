9b275dcc5c3e20a7027e29023e869a5e7d306b7a
Prathamesh Berde <43159826+Pratham1995@users.noreply.github.com>
2021-02-27 13:04:15 -0600

    First Commit

---
+++ src/test/java/com/example/PaymentValidation/PaymentValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaymentValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
